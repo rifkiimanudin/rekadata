109f0c6f377cc1c18361888aad95bf25e2e4da07
rifkiimanudin <rifki.iman@gmail.com>
2020-07-17 19:57:09 +0700

    ok

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -11,10 +11,10 @@
                     <div class="row">
                         <div class="col-lg">
                             <div class="p-5">
-                                <div class="text-center">
+                                <!-- <div class="text-center">
                                     <img src=<?= base_url('assets/img/ok.jpg') ?> width="100" class="rounded-circle">
                                     <h1 class="h4 text-gray-900 mb-4"></h1>
-                                </div>
+                                </div> -->
 
                                 <?= $this->session->flashdata('message'); ?>
 
